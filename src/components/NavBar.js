78bb311fa828bdeefc0ae460fad4a151457e56ca
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-10-11 11:47:53 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -19,7 +19,7 @@ import { Link } from "react-router-dom";
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
               <li className="nav-item">
